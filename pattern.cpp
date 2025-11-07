241970da8992ef9ee5b6bcf1d6d3f17e9882cb2a
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-08 00:23:40 +0600

    erect pyramid+inverted pyramid

--- pattern.cpp
+++ pattern.cpp
@@ -167,9 +167,53 @@ void pattern8(int n){
         
     }
 }
+void pattern9(int n){
+        //       *    
+        //      ***
+        //     *****
+        //    *******
+        //   *********
+        //   *********
+        //    *******
+        //     *****
+        //      ***
+        //       *
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n-i-1 ; j++)
+        {
+            cout <<  " ";
+        }
+        for(int j=1;j<=2*i+1;j++){
+            cout << "*";
+        }
+        for (int j = 1; j <= n-i-1 ; j++)
+        {
+            cout <<  " ";
+        }
+        cout << endl;
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout <<  " ";
+        }
+        for(int j=1;j<=2*n-(2*i+1);j++){
+            cout << "*";
+        }
+        for (int j = 1; j <=i ; j++)
+        {
+            cout <<  " ";
+        }
+        cout << endl;
+        
+    }
+}
 int main(){
 
     int n;
-    pattern8(5);
+    pattern9(5);
     return 0;
 }
