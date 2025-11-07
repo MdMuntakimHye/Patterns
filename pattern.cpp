3b004c4367b47146f0d0e773facbdf3cd43cba15
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-07 21:54:13 +0600

    added pattern 6 with same reversed right angle triangle but with numbers

--- pattern.cpp
+++ pattern.cpp
@@ -100,9 +100,28 @@ void pattern5(int n){
         
     }
 }
+void pattern6(int n){
+        //   1 2 3 4 5 
+        //   1 2 3 4
+        //   1 2 3
+        //   1 2
+        //   1
+    
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n-i ; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+        
+    }
+}
+
 int main(){
 
     int n;
-    pattern5(5);
+    pattern6(5);
     return 0;
 }
