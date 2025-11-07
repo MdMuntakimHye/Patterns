7e78b11ff84ed8cdb8deaa094d7affc34ce292b4
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-07 20:41:34 +0600

    Added pattern 1 which looks like a square. inputting 5 will make it 5x5 . 6 will make it 6x6

--- pattern.cpp
+++ pattern.cpp
@@ -1,3 +1,23 @@
 #include<vector>
 #include<iostream>
 using namespace std;
+
+void pattern1(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+        
+    }
+    
+}
+
+int main(){
+
+    int n;
+    pattern1(5);
+    return 0;
+}
