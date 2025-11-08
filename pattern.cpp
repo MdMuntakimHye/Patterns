df0ab21098b4957cffb2561f07b479d049bcc964
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-09 01:18:11 +0600

    added pattern 13(right triangle but with increasing numbers in each row)

--- pattern.cpp
+++ pattern.cpp
@@ -271,12 +271,21 @@ void pattern12(int n){
     }
     
 }           
-        
+void pattern13(int n){
+    int start = 1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << start << " ";
+            start = start + 1;
+        }
+        cout << endl;
+    }
+}       
     
 
 int main(){
 
     int n;
-    pattern12(5);
+    pattern13(5);
     return 0;
 }
