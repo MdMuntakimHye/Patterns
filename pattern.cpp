600f36ba47ef4f6c21afe745f74bb18fbc7523dd
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2025-11-11 01:38:56 +0600

    added pattern 20 today. cleaned up and revised the other pattern

--- pattern.cpp
+++ pattern.cpp
@@ -212,6 +212,15 @@ void pattern9(int n){
     }
 }
 void pattern10(int n){
+            //  *
+            //  **
+            //  ***
+            //  ****
+            //  *****
+            //  ****
+            //  ***
+            //  **
+            //  *    
     for(int i=1;i<=2*n-1;i++ ){
         int stars = i;
         if(i>n) stars = 2*n-i;
@@ -324,6 +333,11 @@ void pattern15(int n){
     }
 }
 void pattern16(int n){
+            //  A
+            //  BB
+            //  CCC
+            //  DDDD
+            //  EEEEE
     //char cha = 'A';
     for(int i=0;i<n;i++){
         char cha = 'A'+i;
@@ -336,33 +350,37 @@ void pattern16(int n){
     }
 }
 void pattern17(int n){
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= n-i-1 ; j++)
-        {
-            cout <<  " ";
-        }
+//     A    
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA
+    
+    
+    for(int i=0;i<n;i++){
         char ch = 'A';
         int breakpoint = (2*i+1)/2;
+        for(int j=0;j<=n-i-1;j++){
+            cout << " ";
+        }
         for(int j=1;j<=2*i+1;j++){
             cout << ch;
-            
-            if (j<=breakpoint)
-            {
-               ch++;
-            }
+            if(j<=breakpoint) ch++;
             else ch--;
-            
         }
-        for (int j = 1; j <= n-i-1 ; j++)
-        {
-            cout <<  " ";
+        for(int j=0;j<=n-i-1;j++){
+            cout << " ";
         }
         cout << endl;
-        
     }
 }
 void pattern18(int n){
+            //   E
+            //   DE
+            //   CDE
+            //   BCDE
+            //   ABCDE
+
     for (int i = 0; i < n; i++)
     {
         char ch = 'E'-i;
@@ -419,32 +437,41 @@ void pattern19(int n){
     
 }
 void pattern20(int n){
-    // for(int i=1;i<=2*n-1;i++){
-    //     int stars = i;
-    //     if(i>n) stars = 2*n-i;
-    //     for (int j = 1; j <= stars; j++)
-    //     {   
-    //        cout << "*";
-    //     }
-    //     for(int j=0;j<=2*n-(2*i+1);j++){
-    //         cout << "|";
-    //     }
-    //     for (int j = 1; j <= stars; j++)
-    //     {   
-    //        cout << "*";
-    //     }
-        
-    //     cout << endl;
-        
+            //    *        *
+            //    **      **
+            //    ***    ***
+            //    ****  ****
+            //    **********
+            //    ****  ****
+            //    ***    ***
+            //    **      **
+            //    *        *
 
-    // }
-    
-    
+    int spaces = 2*n -2;
+    for(int i=1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout << "*";
+        }
+        //spaces
+        for(int j=1;j<=spaces;j++){
+            cout << " ";
+        }
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout << "*";
+        }
+        cout << endl;
+        if(i<n) spaces -= 2;
+        else spaces +=2;
+    }    
 }
 
 int main(){
 
     int n;
-    pattern20(5);
+    pattern16(5);
     return 0;
 }
