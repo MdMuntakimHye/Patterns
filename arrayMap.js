73095431e18b18d243db1756ae0f0116072a525b
MdMuntakimHye <bsse1256@iit.du.ac.bd>
2026-03-03 03:58:23 +0600

    added filter() using function

--- arrayMap.js
+++ arrayMap.js
@@ -11,11 +11,25 @@ const numbers = [5, 2, 7, 6];
 const doubled = numbers.map(double);
 console.log(doubled);
 
-//you'll want to create a new array containing only the items in the original array that match some test. You can do that using filter()
 function uppercase(city) {
     return city.toUpperCase();
 }
 const cities = ["London", "Liverpool", "Totnes", "Edinburgh"];
 const upcase = cities.map(uppercase);
 console.log(upcase);
+
+
+//you'll want to create a new array containing only the items in the original array that match some test. You can do that using filter()
+//Rule of thumb
+
+//Use map() → “I want to change every item”
+
+//Use filter() → “I want to keep only some items”
+
+function isLong(city) {
+    return city.length > 8;
+}
+const cities2 = ["London", "Liverpool", "Totnes", "Edinburgh"];
+const longer = cities2.filter(isLong);
+console.log(longer);
 
